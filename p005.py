0d0bbef729dd08131a82782abdeda907bbdad7a0
neilpa <neilpa@live.com>
2008-12-28 16:24:25 -0500

    Templates for problems 1-10

---
+++ p005.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+Problem 5
+30 November 2001
+ 
+2520 is the smallest number that can be divided by each of the numbers from 1 to
+10 without any remainder.
+
+What is the smallest number that is evenly divisible by all of the numbers from
+1 to 20?
+
+Answer: ?????
+"""
+
