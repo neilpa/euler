d180c10612e8be357ea66db897ed6e3450e56cfd
Neil Pankey <neilpa@foobar>
2013-12-14 02:25:49 -0800

    Problem 6 solved

---
+++ haskell/6.hs
@@ -0,0 +1,17 @@
+-- Sum square difference
+-- Problem 6
+--
+-- The sum of the squares of the first ten natural numbers is,
+-- 1^2 + 2^2 + ... + 10^2 = 385
+--
+-- The square of the sum of the first ten natural numbers is,
+-- (1 + 2 + ... + 10)^2 = 55^2 = 3025
+--
+-- Hence the difference between the sum of the squares of the first ten natural numbers and the
+-- square of the sum is 3025 − 385 = 2640.
+--
+-- Find the difference between the sum of the squares of the first one hundred natural numbers
+-- and the square of the sum.
+
+xs = [1..100]
+p6 = sum xs ^ 2 - sum [ x^2 | x <- xs ]
