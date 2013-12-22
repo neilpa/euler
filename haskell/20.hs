b56230f0a1ec83f5deb2c4dc90a4f9b5ddc0c7b7
Neil Pankey <neil@wire.im>
2013-12-22 11:45:48 -0800

    Solved 20 in Haskell

---
+++ haskell/20.hs
@@ -0,0 +1,16 @@
+-- Factorial digit sum
+-- Problem 20
+--
+-- n! means n × (n − 1) × ... × 3 × 2 × 1
+--
+-- For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+-- and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+--
+-- Find the sum of the digits in the number 100!
+
+import Data.Char
+
+fact 1 = 1
+fact n = n * fact (n-1)
+
+p20 = sum $ map digitToInt (show $ fact 100)
