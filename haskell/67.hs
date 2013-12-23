21521cb79730b99c58abc92db692bde42ff8d2f4
Neil Pankey <neil@wire.im>
2013-12-22 18:12:03 -0800

    Solved 67 in Haskell

---
+++ haskell/67.hs
@@ -0,0 +1,34 @@
+-- Maximum path sum II
+-- Problem 67
+--
+-- By starting at the top of the triangle below and moving to adjacent numbers on the
+-- row below, the maximum total from top to bottom is 23.
+--
+--       3
+--      7 4
+--     2 4 6
+--    8 5 9 3
+--
+-- That is, 3 + 7 + 4 + 9 = 23.
+
+-- Find the maximum total from top to bottom in triangle.txt (right click and 'Save
+-- Link/Target As...'), a 15K text file containing a triangle with one-hundred rows.
+--
+-- NOTE: This is a much more difficult version of Problem 18. It is not possible to try
+-- every route to solve this problem, as there are 299 altogether! If you could check
+-- one trillion (1012) routes every second it would take over twenty billion years to check
+-- them all. There is an efficient algorithm to solve it. ;o)
+
+import System.IO
+
+myMax r = zipWith max (init r) (tail r)
+p67 = foldr1 (\row acc -> zipWith (+) row (myMax acc))
+
+toInt :: [[String]] -> [[Int]]
+toInt = map (map read)
+
+main = do
+    contents <- readFile "triangle.txt"
+    let solution = p67 $ toInt $ map words (lines contents)
+    putStr (show $ head solution)
+
